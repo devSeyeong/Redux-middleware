f3dca47ccf1b3e55b83fcff0b71689b72f0aed3f
mseyeong <mseyeong3131@mek-ics.com>
2023-04-28 17:50:10 +0900

    Redux thunkJ

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import CounterContainer from "./containers/CounterContainer";
-import PostListContainer from "./containers/PostListContainer";
+import { Route, Routes } from "react-router-dom";
+
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
 import React from "react";
 
 function App() {
-  return <PostListContainer />;
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<PostListPage />} />
+        <Route path=":id" element={<PostPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/PostPage.js
@@ -0,0 +1,10 @@
+import PostContainer from "../containers/PostContainer";
+import React from "react";
+import { useParams } from "react-router";
+
+function PostPage() {
+  const params = useParams();
+  return <PostContainer postId={parseInt(params.id, 10)} />;
+}
+
+export default PostPage;
