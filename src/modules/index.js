53695583c27451b37ec09d3d70fd1c5131356d0a
mseyeong <mseyeong3131@mek-ics.com>
2023-05-02 15:43:51 +0900

    React saga

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,12 @@
+import counter, { counterSaga } from "./counter";
+import posts, { postsSaga } from "./posts";
+
+import { all } from "redux-saga/effects";
 import { combineReducers } from "redux";
-import counter from "./counter";
-import posts from "./posts";
 
 const rootReducer = combineReducers({ counter, posts });
+export function* rootSaga() {
+  yield all([counterSaga(), postsSaga()]);
+}
 
 export default rootReducer;
